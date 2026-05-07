226f90fd40bdccd829284c93393135b67198c988
shawonsarder0012 <shawonsarder0012@gmail.com>
2026-05-07 10:50:39 +0600

    Update config.py to use environment variables
    Refactored configuration to use environment variables for sensitive data.

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
-# config.py
+import os
 
-BOT_TOKEN = "8721728827:AAHjGSBV_iPPyg_iV_sgpxkssG85mklzVTM"
-
-ADMIN_ID = 5002844213
-
+BOT_TOKEN = os.environ.get("8218090305:AAHCU7u2w4PzWt2jRzwht7OUZm2dMl2i4MI")
+ADMIN_ID = int(os.environ.get("ADMIN_ID", "5002844213"))
+ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "nnytt3")
+BOT_USERNAME = os.environ.get("BOT_USERNAME", "bee_honey_money_bot")
 DATABASE_NAME = "taskbot.db"
+
+REFERRAL_COMMISSION = float(os.environ.get("REFERRAL_COMMISSION", "5"))
+REFERRAL_BONUS = float(os.environ.get("REFERRAL_BONUS", "2"))
